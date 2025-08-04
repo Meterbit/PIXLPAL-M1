00ed8a102e2595f6fce67dbfe1866ff25abba9c9
Meterbit <meterbitcyb@gmail.com>
2025-08-04 10:19:57 +0100

    Improve and Debug Project

--- main/main.cpp
+++ main/main.cpp
@@ -31,22 +31,19 @@ extern "C" void app_main(){
     //   printf("Waiting for Internet Connection....\n");
     //   delay(1000);
     // }
-
     // drawOnlinePNG({"https://media.api-sports.io//football//teams//165.png", 3, 17, 5});     // Draw the logo on the top left corner of the screen.
-    // drawOnlinePNG({"https://media.api-sports.io//football//teams//166.png", 45, 17, 5});    // Draw the logo on the top right corner of the screen.
-    //SVG_OnlineImage_t testImage = {"https://raw.githubusercontent.com/woble/flags/refs/heads/master/SVG/3x2/in.svg", 0, 0, 1};
-    //printf("Current Flag: %s\n", testImage.imageLink); 
-    //drawOnlineSVGs(&testImage); // Draw the MTB Logo on the top left corner of the screen.
+    // SVG_OnlineImage_t testImage = {"https://raw.githubusercontent.com/woble/flags/refs/heads/master/SVG/3x2/in.svg", 0, 0, 1};
+    // drawOnlineSVGs(&testImage); // Draw the MTB Logo on the top left corner of the screen.
 
 
-    // while (1){
-    // delay(2000);
-    // // // //printf("Name of Current App is:  %s \n", Applications::currentRunningApp->appName);
-    // // // //Get the total free size of internal SRAM
-    // free_sram = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
-    // //Print the free SRAM size
-    // //printf("#############Free SRAM: %zu bytes\n", free_sram);
-    // //ESP_LOGI(TAG, "Memory: Free %dKiB Low: %dKiB\n", (int)xPortGetFreeHeapSize()/1024, (int)xPortGetMinimumEverFreeHeapSize()/1024);
-    //  }
+    while (1){
+    delay(2000);
+    // // //printf("Name of Current App is:  %s \n", Applications::currentRunningApp->appName);
+    // // //Get the total free size of internal SRAM
+    free_sram = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
+    //Print the free SRAM size
+    printf("#############Free SRAM: %zu bytes\n", free_sram);
+    //ESP_LOGI(TAG, "Memory: Free %dKiB Low: %dKiB\n", (int)xPortGetFreeHeapSize()/1024, (int)xPortGetMinimumEverFreeHeapSize()/1024);
+     }
 
 }
