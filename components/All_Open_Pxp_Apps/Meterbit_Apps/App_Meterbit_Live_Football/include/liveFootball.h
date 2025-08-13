4a697efa84ea7a23712be946e3c1db3755b81791
Meterbit <meterbitcyb@gmail.com>
2025-08-13 22:54:32 +0100

    Restore Files

---
+++ components/All_Open_Pxp_Apps/Meterbit_Apps/App_Meterbit_Live_Football/include/liveFootball.h
@@ -0,0 +1,30 @@
+#ifndef LIVE_FOOTBALL_H
+#define LIVE_FOOTBALL_H
+
+// #include <stdio.h>
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+// #include "encoder.h"
+// #include "button.h"
+
+#define FIXTURES_ENDPOINT 0
+#define STANDINGS_ENDPOINT 1
+#define LIVE_MATCHES_ENDPOINT 2
+
+static const char liveFootbalAppRoute[] = "5/0";
+
+// Your API-Football subscription key from RapidAPI
+static const char *API_KEY = "a2e0674bdfb9d68a5e001e391bcb5160";
+
+struct LiveFootball_Data_t {
+  uint8_t endpointType = 0; // 0: Fixture; 1: Standings
+  uint16_t leagueID = 39; // Default league ID for API-Football
+  char userAPI_Token[100] = {0}; // User's API token for authentication
+};
+
+extern LiveFootball_Data_t liveFootballData;
+extern TaskHandle_t liveFootball_Task_H;
+extern void liveFootball_App_Task(void *arguments);
+
+#endif
